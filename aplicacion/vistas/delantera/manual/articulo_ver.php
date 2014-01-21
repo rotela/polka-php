bb818a5c768a530c6e567eca5cae4d0f388e7b8c
joeblack0 <ricksystems@gmail.com>
2014-01-21 11:54:43 -0300

    algunos archivos eliminados

--- aplicacion/vistas/delantera/manual/articulo_ver.php
+++
@@ -1,8 +0,0 @@
-<div class="panel panel-default">
-	<div class="panel-heading">
-		<?php echo isset($titulo) ? $titulo : 'sin titulo'; ?>
-	</div>
-	<div class="panel-body">
-	<?php echo isset($contenido) ? $contenido : 'sin contenido'; ?>
-	</div>
-</div>
