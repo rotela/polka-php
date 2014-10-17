ca38fa647830890b8a51661ae5dd2d9679703444
joeblack0 <ricksystems@gmail.com>
2014-10-16 23:14:12 -0300

    agregando algunos comentarios

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-
+/*definimo tipo reportes de errores*/
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
+/*defino la zona horaria*/
 date_default_timezone_set('America/Asuncion');
-
+/*defino los directorios principales*/
 define('SD', DIRECTORY_SEPARATOR);
 define('RAIZ', realpath(dirname(__FILE__)) . SD);
 define('SISTEMA', 'sistema' . SD);
