4eacbd3596986af9ac47da1526eb1e14d2e598c7
joeblack0 <ricksystems@gmail.com>
2015-06-20 18:46:39 -0400

    ajustes de lineas, se repetian algunas cosas

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 <?php
 
-/*definimo tipo reportes de errores*/
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-/*defino la zona horaria*/
-date_default_timezone_set('America/Asuncion');
-/*defino los directorios principales*/
-
 /*defino tipo reporte de errores*/
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
 /*defino zona horaria*/
 date_default_timezone_set('America/Asuncion');
-/*defino directorios*/
 
+/*defino directorios*/
 define('SD', DIRECTORY_SEPARATOR);
 define('RAIZ', realpath(dirname(__FILE__)) . SD);
 define('SISTEMA', 'sistema' . SD);
 define('NUCLEO', SISTEMA . 'nucleo' . SD);
+
 /*incluyo la clase de autocargas*/
 require NUCLEO . 'PK_Autocargar.php';
+
 /*intento inicializar el sistema, de lo contrario muestro un mensaje*/
 try {
     sistema\nucleo\PK_Disparador::iniciar();
